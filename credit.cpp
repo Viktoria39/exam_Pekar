0bb1b74e696a93fc38bf5e6f071ca762152a4669
Vika Pekar <pekarviktoria39@gmail.com>
2020-05-05 23:05:36 +0300

    CheckInput

--- credit.cpp
+++ credit.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 using namespace std;
 
+void CheckInput(char NumberCard[], const int SIZE_CARD);
+
 int main()
 {
 	const int SIZE_CARD = 16;
 	char NumberCard[SIZE_CARD + 1]{};
 
+    CheckInput(NumberCard, SIZE_CARD);
+
 	return 0;
+}
+
+void CheckInput(char NumberCard[], const int SIZE_CARD)
+{
+    bool exit = true;
+
+    do
+    {
+        cout << "Enter number card 16 characters\n";
+        cin.getline(NumberCard, SIZE_CARD + 1);
+
+        NumberCard[SIZE_CARD] = '\0';
+
+        for (size_t i = 0; i < SIZE_CARD; i++)
+        {
+            if (NumberCard[i] >= 0 && !(NumberCard[i] > ':') && !(NumberCard[i] < '/'))
+            {
+                if (i == SIZE_CARD - 1)
+                    exit = false;
+            }
+            else
+            {
+                cout << "Incorrect input. Please try again\n";
+                cin.clear();
+                while (cin.get() != '\n');
+                break;
+            }
+        }
+    } while (exit);
 }
